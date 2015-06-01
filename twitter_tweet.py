e716ef72d9a74e80db04dbf3458c3201f85ab270
Nilay <nilay.shah911@gmail.com>
2015-06-01 00:16:27 -0700

    followers
    hw4

--- twitter_tweet.py
+++ twitter_tweet.py
@@ -3,35 +3,39 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from pymongo import MongoClient
 import os
 
 try:
-    os.remove('twitter.txt')
+    client=MongoClient()
+    client.drop_database("twitterdb")
 except OSError:
     pass
 
-ckey = "4lAbO1GoI2U0IqAy1SDHncbZd"
-csecret = "9VKBmQCjzvfa9J44UeMrczuGehoiwSLSx6f5tXB4cJOfEZPoVI"
-atoken= "2902111402-6njYp4DsIQZA8JkqKKIJbJ977Xb1qj9AXOrLPhz"
-asecret= "vuGahDFX7T7PV2RJrLOcqWIjhVVaWqi5Un5oJeyV96EhI"
+ckey = "IMUFGw7o7Sm9Ygx9YhpgEelO7"
+csecret = "BAli9zGOadDXzJ6boR9psKyeJZrzVb6W2ka9hy9H9SjWmStwgo"
+atoken= "624867037-NxR60aaB6jUcg6Jlj7uZGdWw7od023EpAO76pP39"
+asecret= "rt2lpUZpxVMEuBGO3CFvp2tu9cIQco3fI1Jnr1Jnn3vSs"
 
 class listener (StreamListener):
 
     def on_data(self,data):
         try:
-
+            client=MongoClient()
+            db=client.twitterdb
             time=data.split('"created_at":"')[1].split('","id')[0]
             tweet=data.split(',"text":"')[1].split('","source')[0]
             name=data.split(',"name":"')[1].split('","screen_name')[0]
             location=data.split(',"location":"')[1].split('","url')[0]
             time_zone=data.split(',"time_zone":"')[1].split('","geo_enabled')[0]
             lang=data.split(',"lang":"')[1].split('","contributors_enabled')[0]
-            Data='Created at:: '+time+'\n'+'Tweet:: '+tweet+'\n'+'Name:: '+name+'\n'+'Location:: '+location+'\n'+'Time_Zone:: '+time_zone+'\n'+'Language:: '+lang+'\n'
+            Data={"Created at:: ":time,"Tweet:: ":tweet,"Name:: ":name,"Location:: ":location,"Time_Zone:: ":time_zone,"Language:: ":lang}
             print (Data)
-            output=open('twitter.txt','a')
-            output.write(Data)
-            output.write('\n')
-            output.close()
+            db.twitterdb.insert(Data)
+            #output=open('twitter.txt','a')
+            #output.write(Data)
+            #output.write('\n')
+            #output.close()
             return True
         except BaseException, e:
             print 'error', str(e)
@@ -42,4 +46,4 @@ class listener (StreamListener):
 auth=OAuthHandler(ckey,csecret)
 auth.set_access_token(atoken,asecret)
 twitterStream=Stream(auth,listener())
-twitterStream.filter(track=["google"])
+twitterStream.filter(track=["twitter"])
